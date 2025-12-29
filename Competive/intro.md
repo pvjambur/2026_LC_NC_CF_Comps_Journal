a74696915f8638fec4ddce7414864012c6cb2054
pvjambur <145439975+pvjambur@users.noreply.github.com>
2025-12-30 01:16:20 +0530

    New object

---
+++ Competive/intro.md
@@ -0,0 +1,1 @@
+## Comps prog (CF & LC)
---
+++ General_DSA/Binary_Trees/bt.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+void inorder(Node* root){
+    if (root == nullptr) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+
+    inorder(root);
+
+    return 0;
+}
